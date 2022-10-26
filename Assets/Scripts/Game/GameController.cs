a707b0a9603317e95b2a4ba1520d1b849407758e
Sladkiy Eduard <nikcxprojects@gmail.com>
2022-10-26 19:11:51 +0300

    init

---
+++ Assets/Scripts/Game/GameController.cs
@@ -0,0 +1,169 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Game
+{
+    class GameController : MonoBehaviour
+    {
+        public static GameController Instance;
+        public Texture2D Texture;
+        public SpriteRenderer FildRenderer;
+        public Image[] liveImages;
+        public Sprite liveSprite;
+        public Sprite deadSprite;
+
+        private GameXonix _gameXonix;
+
+        private float _levelTime;
+        public bool TimeIsUp;
+
+        #region UI_ELEMENTS
+        public Text Level;
+        public Text Fill;
+        public Text Score;
+
+        public RectTransform GameOver;
+        #endregion
+
+        #region SWIPE_DETECTION
+        private Vector3 _touchPosition;
+        private float _swipeResistanceX = 50.0f;
+        private float _swipeResistanceY = 100.0f;
+        #endregion
+        [SerializeField] private AudioClip _clickSound;
+
+        public void PlaySound()
+        {
+            AudioManager.instance.PlayAudio(_clickSound);
+        }
+        void Awake()
+        {
+            Instance = this;
+        }
+
+        void Start()
+        {
+            GameStart();
+        }
+
+        void Update()
+        {
+            CountDownLevelTime();
+            Move();
+        }
+
+        private void GameStart()
+        {
+            SetUI(true);
+            SetTimer();
+            _gameXonix = new GameXonix();
+            StartCoroutine(_gameXonix.Go());
+        }
+
+        public void SetTimer()
+        {
+            TimeIsUp = false;
+            _levelTime = 60;
+        }
+
+        public void SetUI(bool isVisible)
+        {
+            GameOver.gameObject.SetActive(!isVisible);
+        }
+
+
+        private void Move()
+        {
+#if UNITY_EDITOR 
+            if (!GameXonix.gameOverOrPause.IsPaused() && !GameXonix.gameOverOrPause.IsGameOver())
+            {
+                if (Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    GameXonix.xonix.SetDirection(GameXonix.RIGHT);
+                }
+                else if (Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    GameXonix.xonix.SetDirection(GameXonix.UP);
+                }
+                else if (Input.GetKeyDown(KeyCode.LeftArrow))
+                {
+                    GameXonix.xonix.SetDirection(GameXonix.LEFT);
+                }
+                else if (Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    GameXonix.xonix.SetDirection(GameXonix.DOWN);
+                }
+            }
+#endif
+
+#if UNITY_ANDROID
+            if (!GameXonix.gameOverOrPause.IsPaused() && !GameXonix.gameOverOrPause.IsGameOver())
+            {
+                if(Input.GetMouseButtonDown(0))
+                {
+                    _touchPosition = Input.mousePosition;
+                }
+
+                if (Input.GetMouseButtonUp(0))
+                {
+                    var deltaSwipe = _touchPosition - Input.mousePosition;
+                    if(Mathf.Abs(deltaSwipe.x) > _swipeResistanceX)
+                    {
+                        //Swipe on the X axis
+                        GameXonix.xonix.SetDirection((deltaSwipe.x < 0) ? GameXonix.RIGHT : GameXonix.LEFT);
+                    }
+                    else if (Mathf.Abs(deltaSwipe.y) > _swipeResistanceY)
+                    {
+                        //Swipe on the Y axis
+                        GameXonix.xonix.SetDirection((deltaSwipe.y < 0) ? GameXonix.DOWN : GameXonix.UP);
+                    }
+                }
+            }
+#endif
+        }
+
+        private void CountDownLevelTime()
+        {
+            if (GameXonix.gameOverOrPause.IsPaused() || GameXonix.gameOverOrPause.IsGameOver()) return;
+            if ((_levelTime > 0))
+            {
+                _levelTime -= Time.deltaTime;
+            }
+            else
+                TimeIsUp = true;
+        }
+
+        public void OnPlayButton()
+        {
+            SceneManager.LoadScene("Game", LoadSceneMode.Single);
+        }
+
+        public void SetLivesCount(int live)
+        {
+            int deadCount = 3 - live;
+            for (int i = 0; i < deadCount; i++)
+                liveImages[i].sprite = deadSprite;
+        }
+
+        public void SetFillAmount(int fillArea)
+        {
+            Fill.text = Mathf.Round(fillArea).ToString() + " %";
+        }
+
+        public void SetLevelNum(int level)
+        {
+            Level.text = $"Level: {level}";
+        }
+
+        public void SetScoreNum(int score)
+        {
+            Score.text = $"Score: {score}";
+        }
+
+        public void BackToMenu()
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+}
---
+++ Assets/Scripts/UIMenu.cs
@@ -0,0 +1,141 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIMenu : MonoBehaviour
+{
+    [Header("Settings References")]
+    [SerializeField] private Text _audioText;
+    [SerializeField] private Text _musicText;
+
+    [Header("Scores")]
+    [SerializeField] private Transform _scoreWindowTransform;
+    [SerializeField] private GameObject _scorePrefab;
+    private List<GameObject> _scoreInitialied = new List<GameObject>();
+    [SerializeField] private AudioClip _clickSound;
+
+    public void PlaySound()
+    {
+        AudioManager.instance.PlayAudio(_clickSound);
+    }
+
+    private void Awake()
+    {
+        AudioLoader();
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    /// <summary>
+    /// The method includes the main menu
+    /// </summary>
+    public void Home()
+    {
+        if (_scoreInitialied.Count == 0)
+            return;
+
+        foreach (GameObject score in _scoreInitialied)
+            Destroy(score);
+
+        _scoreInitialied.Clear();
+    }
+
+    public void ViewScores()
+    {
+        List<int> scores = Database.instance.GetScoresDatabase();
+
+        if (scores.Count == 0)
+            return;
+
+        for (int i = scores.Count - 1; i > -1; i--)
+        {
+            GameObject scoreObj = Instantiate(_scorePrefab, _scoreWindowTransform);
+            scoreObj.GetComponent<RectTransform>().localScale = Vector3.one;
+
+            Text scoreText = scoreObj.GetComponent<Text>();
+            scoreText.text = $"{scores.Count - i}. {scores[i]} points";
+
+            _scoreInitialied.Add(scoreObj);
+        }
+    }
+
+    /// <summary>
+    /// Clears information about records
+    /// </summary>
+    public void ClearScores()
+    {
+        foreach (GameObject score in _scoreInitialied)
+            Destroy(score);
+
+        _scoreInitialied.Clear();
+    }
+
+    #region Settings
+    public void ChangeAudio()
+    {
+        if (PlayerPrefs.GetString(PrefsKey.audio) == "Enable")
+        {
+            PlayerPrefs.SetString(PrefsKey.audio, "Disable");
+            AudioManager.instance.EnableAudio(false);
+            _audioText.text = "SOUND: OFF";
+        }
+        else
+        {
+            PlayerPrefs.SetString(PrefsKey.audio, "Enable");
+            AudioManager.instance.EnableAudio(true);
+            _audioText.text = "SOUND: ON";
+        }
+    }
+
+    public void ChangeMusic()
+    {
+        if (PlayerPrefs.GetString(PrefsKey.music) == "Enable")
+        {
+            PlayerPrefs.SetString(PrefsKey.music, "Disable");
+            AudioManager.instance.EnableMusic(false);
+            _musicText.text = "MUSIC: OFF";
+        }
+        else
+        {
+            PlayerPrefs.SetString(PrefsKey.music, "Enable");
+            AudioManager.instance.EnableMusic(true);
+            _musicText.text = "MUSIC: ON";
+        }
+    }
+
+    private void AudioLoader()
+    {
+        if (!PlayerPrefs.HasKey(PrefsKey.audio)) PlayerPrefs.SetString(PrefsKey.audio, "Enable");
+        if (!PlayerPrefs.HasKey(PrefsKey.vibration)) PlayerPrefs.SetString(PrefsKey.vibration, "Enable");
+        if (!PlayerPrefs.HasKey(PrefsKey.music)) PlayerPrefs.SetString(PrefsKey.music, "Enable");
+
+        if (PlayerPrefs.GetString(PrefsKey.audio) == "Enable")
+        {
+            AudioManager.instance.EnableAudio(true);
+            _audioText.text = "SOUND: ON";
+        }
+        else
+        {
+            AudioManager.instance.EnableAudio(false);
+            _audioText.text = "SOUND: OFF";
+        }
+
+        if (PlayerPrefs.GetString(PrefsKey.music) == "Enable")
+        {
+            AudioManager.instance.EnableMusic(true);
+            _musicText.text = "MUSIC: ON";
+        }
+        else
+        {
+            AudioManager.instance.EnableMusic(false);
+            _musicText.text = "MUSIC: OFF";
+        } 
+    }
+
+    #endregion
+}
